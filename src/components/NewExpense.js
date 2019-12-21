adf38cb6c089afbafeb62d73a60d407fcefade3a
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2019-12-21 13:53:44 +0100

    handle change on form

--- src/components/NewExpense.js
+++ src/components/NewExpense.js
@@ -1,45 +1,79 @@
 import React,{Component} from 'react';
 
 class NewExpense extends Component{
+    state = {
+        expense: {
+            type: '',
+            name: '',
+            date: '',
+            amount: 0, 
+        }
+    }
 
+    handleChange = e => {
+        console.log(e.target.name + ': ' + e.target.value);
+
+        this.setState({
+            expense : {
+                ...this.state.expense,
+                [e.target.name] : e.target.value
+            }
+        });
+    } 
     render(){
         return (
             <div className="">
                 <form>
                     <div className="form-group row">
                        
-                            <label for="type">Type:</label>
-                        <select className="form-control">
+                            <label>Type:</label>
+                        <select 
+                            className="form-control" 
+                            name="type"
+                            onChange={this.handleChange}
+                            value={this.state.expense.type}
+                            >
                             <option value="chooseOne">Choose one...</option>
                             <option value="card">Card</option>
                             <option value="cash">Cash</option>
                             <option value="cryptocoin">Cryptocoin</option>
                             <option value="other">Other</option>
                         </select>
-                        <span for="name">Name:</span>
+                        <span>Name:</span>
                         <input 
                             className="form-control"
                             placeholder="Expense Name" 
                             type="text" 
-                            name="name" />
+                            name="name"
+                            onChange={this.handleChange}
+                            value={this.state.expense.name}    
+                        />
                     </div>
 
                     <div className="form-group row">
-                        <span for="date">Date:</span>
+                        <span>Date:</span>
                         <input 
                             className="form-control" 
                             type="date" 
-                            name="date" />
+                            name="date" 
+                            onChange={this.handleChange}
+                            value={this.state.expense.date}
+                        />
                     </div>
                     <div className="form-group row">
-                        <span for="amount">Amount:</span>
+                        <span>Amount:</span>
                         <input 
                             className="form-control" 
                             type="text" 
                             placeholder="Expense Amount"
-                            name="amount" />
+                            name="amount" 
+                            onChange={this.handleChange}  
+                            value={this.state.expense.amount}  
+                        />
                     </div>
-                    <button type="submit" className="btn btn-block btn-success">Add Expense</button>
+                    <button 
+                        type="submit" 
+                        className="btn btn-block btn-success">Add Expense</button>
                 </form>
             </div>
         );
